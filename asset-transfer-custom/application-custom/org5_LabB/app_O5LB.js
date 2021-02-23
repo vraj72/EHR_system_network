ef7fd0288aa74715471c1264682038c11e7fe131
vraj72 <virajtandel72@gmail.com>
2021-02-23 13:15:06 +0530

    LAb A and B login page done

--- asset-transfer-custom/application-custom/org5_LabB/app_O5LB.js
+++ asset-transfer-custom/application-custom/org5_LabB/app_O5LB.js
@@ -1,11 +1,27 @@
 import express from 'express';
-//server imports
-import patient from './routes/patient.js';
+import exphbs  from 'express-handlebars';
+import path from 'path';
+import cors from 'cors';
+const __dirname = path.resolve(path.dirname(''));
 const app = express();
 app.use(express.json());
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+}));
+
 //server variables
 const port = 8040;
-app.use('/patient', patient);
+
+app.get('/',function(req, res){
+    res.render('login.hbs',{layout: false});
+});
+app.use(express.static(path.join(__dirname,'/public/css')));
 app.listen(port, () => {
     console.info(`Server running on port ${port}`);
+    console.log(__dirname);
 });
